b21ddd8d2b27f923e9bf0c18b6e6203698b744f0
hikimochi <rhikimochi@gmail.com>
2021-07-07 21:28:58 +0900

    Fix for review.

--- src/screens/HinomeScreen.tsx
+++ src/screens/HinomeScreen.tsx
@@ -29,12 +29,14 @@ export const HinomeScreen: React.FC<Props> = ({ navigation }: Props) => {
 
   const onStop = () => {
     setAlbum(null);
+    // firebaseの通知コレクションを消しに行く処理
     console.log(album);
   };
 
   const timeFormat = (date: Date) => {
-    const month = date.getMonth();
-    const day = date.getDay();
+    console.log(date);
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
     const hours = date.getHours();
     const minutes = date.getMinutes();
     return `${month}月${day}日${hours}時${minutes}分`;
