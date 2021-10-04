eee968adea30cbeb76a41808e8250b875f031f39
hikimochi <rhikimochi@gmail.com>
2021-10-04 17:35:29 +0900

    犬

--- src/navigation/AuthStackNavigator.tsx
+++ src/navigation/AuthStackNavigator.tsx
@@ -3,7 +3,6 @@ import { View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 /* components */
 import { HelpButton } from '../components/HelpButton';
-import { LogOutButton } from '../components/LogOutButton';
 /* screens */
 import { LogInScreen } from '../screens/LogInScreen';
 import { SignUpScreen } from '../screens/SignUpScreen';
